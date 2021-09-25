b75186fcdb87b09d8b80d8e40a1e7f6ae16b80e6
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2021-09-26 00:05:24 +0530

    Drago

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+const intents = new Discord.Intents(32767)
+const client = new Discord.Client({ intents, partials: ["CHANNEL", "MESSAGE", "GUILD_MEMBER", "REACTION"] })
+require('dotenv').config()
+
+module.exports = client
+
+const fs = require('fs')
+
+client.slash_commands = new Discord.Collection()
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection();
+
+['common_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord)
+})
+
+require("./handlers/slash_handler")
+
+client.login(process.env.DISCORD_TOKEN)
